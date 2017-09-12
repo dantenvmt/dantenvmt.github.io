af9c8a9b58cc9329acb607642b442aea6f21dc0d
dantenvmt <31034397+dantenvmt@users.noreply.github.com>
2017-09-12 18:14:37 +0700

    asdfasdfasd

--- link-image/DOM/script.js
+++ link-image/DOM/script.js
@@ -1,4 +1,52 @@
 function changeFontSize(x){
-			document.getElementsByClassName('text')
-			text.style.fontsize = '50px';
+		x = parseInt(x);
+	 
+		var text = document.getElementsByClassName('text')
+		for (var i=0 ; i < text.length ; i++) {
+			text[i].style.fontSize = x +'px';
+		}
+		
 	}
+
+function increaseFontSize(paragraph){
+    var div = document.getElementById(paragraph);
+    var currentFont = div.style.fontSize;
+
+    if (parseInt(currentFont) + 1 > 30) {
+    	alert('the font size if not applicable')
+    }
+    else{
+    	    div.style.fontSize = parseInt(currentFont) + 1 + "px";
+    }
+  
+}
+
+function decreaseFontSize(paragraph){
+
+    var div = document.getElementById(paragraph);
+    var currentFont = div.style.fontSize;
+    if (parseInt(currentFont) - 1 < 10) {
+    	alert('the font size if not applicable')
+    }
+    else {
+    	div.style.fontSize = parseInt(currentFont) - 1 + "px";
+    }
+}
+function changeColor(){
+	var color = document.getElementsByClassName('text')
+		for (var i = 0; i < color.length; i++) {
+			if (i == 0) {
+				color[i].style.color = 'blue'
+			}
+			if (i == 1){
+				color[i].style.color ='yellow'
+			}
+			if (i == 2){
+				color[i].style.color = 'red'
+			}
+			if (i == 3) {
+				color[i].style.color = 'green'
+			}
+		}
+
+}
